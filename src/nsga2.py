e131583cff684542b965fddb6dafbd318ae46828
m-ello <sarateanualex4@gmail.com>
2026-01-05 15:11:37 +0200

    src: script: tournament selection + main start

--- src/nsga2.py
+++ src/nsga2.py
@@ -15,7 +15,8 @@ BOUNDS = np.array([
 density = 2400      # kg/m3 densitatea betonului
 cost = 135          # $/m3
 
-def evaluate(w,h,t):
+def evaluate(indidivual):
+    w,h,t = individual
     volume = w*h*t
     mass = volume * density
     cost_total = volume * cost
@@ -29,9 +30,17 @@ def evaluate(w,h,t):
 def dominates(x,y):
     return all(a<=b for a,b in zip(x,y)) and any(a < b for a,b in zip(x,y))
 
-def tournament_selection():
 
-def crossover():
+def tournament_selection(population, fitness):
+    i,j = random.sample(range(len(population)), 2)
+    if dominates(fitness[i], fitness[j]):
+        return population[i]
+    if dominates(fitness[j], fitness[i]):
+        return population[j]
+    return population[i] if random.random()<0.5 else population[j]
+    
+    
+def crossover(parent1, parent2):
 
 def mutation():
 
@@ -40,4 +49,16 @@ def mutation():
 #####################
 for i in range(population_size)   
     population[i] = np.array([random.uniform(*BOUNDS[j])for j in range(3)])
+    
+for _ in range(GENERATIONS):
+    fitness = [evaluate(individual) for individual in population]
+    offspring = []
+    
+    while len(offspring)<population_size:
+        parent1 = tournament_selection(population,fitness)
+        parent2 = tournament_selection(population,fitness)
+        child1, child2 = crossover(parent1, parent2)
+        offspring.append(mutation(child1))
+        offspring.append(mutation(child2))
+        
     
